227f6c20e11b9857ef18d8dfcea138c3b1c9fa5c
Wajih Ouertani <wajih.ouertani@gmail.com>
2021-03-13 08:08:01 +0100

    udpate DataFrame (to store extraction and description time)

--- src/dataStructures.h
+++ src/dataStructures.h
@@ -3,15 +3,21 @@
 
 #include <vector>
 #include <opencv2/core.hpp>
+#include <string>
 
 
 struct DataFrame { // represents the available sensor information at the same time instance
-    
+
     cv::Mat cameraImg; // camera image
-    
+
     std::vector<cv::KeyPoint> keypoints; // 2D keypoints within camera image
     cv::Mat descriptors; // keypoint descriptors
     std::vector<cv::DMatch> kptMatches; // keypoint matches between previous and current frame
+
+    std::string image_name;
+    double extraction_time;
+    double description_time;
+
 };
 
 
